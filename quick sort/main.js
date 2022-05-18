793c7ceed80d107ae61e16487a148d1a83eeb300
Nikolay <dathc@bk.ru>
2022-05-19 02:01:09 +0300

    quick sort

---
+++ quick sort/main.js
@@ -0,0 +1,15 @@
+arr = [4,2,8,7,1,3]
+
+const q =(arr) => {
+    if(arr.length < 2) {
+        return arr
+    }
+
+    const mid = Math.floor(arr.length / 2)
+
+    const left = arr.filter(val => val < arr[mid])
+    const right = arr.filter(val => val > arr[mid])
+    return [...q(left), arr[mid], ...q(right)]
+}
+
+console.log(q(arr))
